9cf20bb2f45e95091760fa7c4d67fc2230973255
vituan1912000 <vituan1912000@gmail.com>
2021-03-23 00:41:03 +0700

    tut8

---
+++ app/src/main/java/com/example/tut8/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tut8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
